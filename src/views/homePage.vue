333aa49133a1827a965bc20902539345e470f08f
he fan <fanhe666rbl>
2020-12-24 15:10:24 +0800

    123

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -87,7 +87,7 @@
         <el-row class="keyword_list" v-for="(o, index) in this.hotKeywords" :key="index">
           <el-image :src="require('@/assets/home_icon/Num-'+(index+1)+'.png')" class="num-icon-kw"></el-image>
           <el-link class="keyword" :underline="false" @click="goKwSearch(o.keyword)">{{o.keyword}}</el-link>
-          <span class="keyword_views">{{o.view}}</span>
+          <span class="keyword_views">{{o.citedNum}}</span>
         </el-row>
       </div>
     </div>
